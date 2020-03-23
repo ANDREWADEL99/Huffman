2159bc54cbae3be8f81005ad6eebf38103c3d62e
ANDREWADEL99 <48730099+ANDREWADEL99@users.noreply.github.com>
2020-03-24 00:09:55 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -13,6 +13,25 @@ class HeapNode():
              self.freq = freq
              self.left = None
              self.right = None
+         
+         
+         def __cmp__(self, other):
+             if(other == None):
+                 return -1
+             if(isinstance(other, HeapNode)):
+                 return -1
+             return self.freq > other.freq
+        
+        
+         def __lt__(self, other):
+             return self.freq < other.freq
+
+         def __eq__(self, other):
+             if(other == None):
+                 return False
+             if(not isinstance(other, HeapNode)):
+                 return False
+             return self.freq == other.freq
 
 def main():
      f=open("Huffman.txt", "r");
@@ -26,32 +45,26 @@ def main():
          else:
              count[x] = 1
      total = sum(count.values());
+     
      for value in count.keys():
          count[value] = count[value]/total;
-     print(count)
+     #print(count)
      
-     #syntax
+     list1 = []
+     heapq.heapify(list1)
+     
      for key in count:
          node = HeapNode(key, count[key])
-         heap = heapq.heappush(node)
-         
-     while(len(heap)>1):
-         node1 = heapq.heappop(heap)
-         node2 = heapq.heappop(heap)
+         print(node)
+         heapq.heappush(list1 ,node)
+                  
+     while(len(list1)>1):
+         node1 = heapq.heappop(list1)
+         node2 = heapq.heappop(list1)
          merged = HeapNode(None, node1.freq + node2.freq)
          merged.left = node1
          merged.right = node2
-         heapq.heappush(heap, merged)
+         heapq.heappush(list1, merged)
          
-     def makeCode(self, root, CurrentCode):
-         if(root == None):
-             return
-         if(root.char != None):
-            #to do 
-             return
-         self.makeCode(root.left, CurrentCode + '0')
-         self.makeCode(root.right, CurrentCode + '1')
-        
-     print(heap)    
   
 main()
