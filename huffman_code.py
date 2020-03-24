73cb507611fbf2148409c35f98775cb0471bbd2d
ANDREWADEL99 <48730099+ANDREWADEL99@users.noreply.github.com>
2020-03-24 03:00:03 +0200

    Done

--- huffman_code.py
+++ huffman_code.py
@@ -33,7 +33,7 @@ class HeapNode():
                  return False
              return self.freq == other.freq
          
-def heap_to_code(root,currentCode,codes,reverse):
+def heapCoding(root,currentCode,codes,reverse):
     if (root == None):
         return
 
@@ -41,22 +41,25 @@ def heap_to_code(root,currentCode,codes,reverse):
         codes[root.char]= currentCode
         reverse[currentCode]=root.char
 
-    heap_to_code(root.left,currentCode + "0",codes,reverse)
-    heap_to_code(root.right,currentCode + "1",codes,reverse)                   
+    heapCoding(root.left,currentCode + "0",codes,reverse)
+    heapCoding(root.right,currentCode + "1",codes,reverse)                   
 
 def main():
+     #read from file
      f=open("Huffman.txt", "r");
      if f.mode == 'r':
          text =f.read();
-         print(text);
-     count = {};   
+         #print(text);
+     count = {};
+     #get count for all char
      for x in text:
          if  x in count.keys():
-             count[x]+=1
+             count[x]+= 1
          else:
              count[x] = 1
+             
      total = sum(count.values());
-     
+     #get freq
      for value in count.keys():
          count[value] = count[value]/total;
      #print(count)
@@ -64,11 +67,12 @@ def main():
      list1 = []
      heapq.heapify(list1)
      
+     #creating heap
      for key in count:
          node = HeapNode(key, count[key])
-         print(node.char)
          heapq.heappush(list1 ,node)
-                  
+     
+     #sorting it             
      while(len(list1)>1):
          node1 = heapq.heappop(list1)
          node2 = heapq.heappop(list1)
@@ -82,13 +86,13 @@ def main():
      currentCode ="";
      codes={};
      reverse={};
-     heap_to_code(root,currentCode,codes,reverse);
+     heapCoding(root,currentCode,codes,reverse);
      
      encoded_text =""
      for character in text:
          encoded_text += codes[character]
          
-     print(encoded_text)
+     #print(encoded_text)
      #Decode the encoded text
      code=""
      decode_text=""
@@ -98,7 +102,8 @@ def main():
              character =reverse[code]
              decode_text +=character
              code = ""
-             
-     print(decode_text)
+     file1 = open("result.txt","w")        
+     file1.write(decode_text)
+     file1.close()
 
 main()
